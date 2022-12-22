530cbdf970cfe478d5ca57f9141bd722bcca9802
thiagoc <thiagoc1415@gmail.com>
2022-12-22 10:32:33 -0300

    feat: Category Endpoints

---
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,29 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPost
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        var category = new Category
+        {
+            Name = categoryRequest.Name,
+            CreatedBy = "Teste",
+            CreatedOn = DateTime.Now,
+            UpdatedBy = "Teste",
+            UpdatedOn = DateTime.Now
+        };
+        context.Categories.Add(category);
+        context.SaveChanges();
+
+        return Results.Created($"/categories/{category.Id}", category.Id);
+    }
+
+}
---
+++ src/Endpoints/Categories/CategoryPut.cs
@@ -0,0 +1,26 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPut
+{
+    public static string Template => "/categories/{id}";
+    public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
+    public static Delegate Handle => Action;
+
+
+    public static IResult Action([FromRoute]Guid Id,CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        var category = context.Categories.Where(Categories=> Categories.Id == Id).FirstOrDefault();
+        category.Name = categoryRequest.Name;
+        category.Active = categoryRequest.Active;
+        category.UpdatedBy = "Editado";
+        category.UpdatedOn = DateTime.Now;
+
+        context.SaveChanges();
+        return Results.Ok();
+    }
+
+}
